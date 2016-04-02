4f56b54f04b26f70bdd6ce46befe390526ecee4b
Dmitri Khrebtukov <khr128@gmail.com>
2016-04-02 11:57:47 -0700

    Create project. Add KGRangeViewController.

---
+++ KGraghRange/KGRangeViewController.h
@@ -0,0 +1,13 @@
+//
+//  KGRangeViewController.h
+//  KGraghRange
+//
+//  Created by khr on 4/2/16.
+//  Copyright © 2016 khr. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface KGRangeViewController : NSViewController
+
+@end
---
+++ KGraghRange/KGRangeViewController.m
@@ -0,0 +1,24 @@
+//
+//  KGRangeViewController.m
+//  KGraghRange
+//
+//  Created by khr on 4/2/16.
+//  Copyright © 2016 khr. All rights reserved.
+//
+
+#import "KGRangeViewController.h"
+
+@interface KGRangeViewController ()
+
+@end
+
+@implementation KGRangeViewController
+
+- (void)viewDidLoad {
+
+  [super viewDidLoad];
+  self.view.layer.backgroundColor = [NSColor redColor].CGColor;
+
+}
+
+@end
